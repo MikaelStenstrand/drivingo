304d2aca12886b264c63b389aba5f4e636f87789
Mikael Stenstrand <stenstrand.mikael@gmail.com>
2020-03-28 08:39:04 +0200

    add debug button

---
+++ Assets/Scripts/Debugging/TestDebugging.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDebugging : MonoBehaviour
+{
+    public void TestDebuggingFunction()
+    {
+        Debug.Log("DEBUG PRESSED");
+    }
+    
+}
