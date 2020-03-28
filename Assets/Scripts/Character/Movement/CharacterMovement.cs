b0f75ece5fa3ddf1e79705bfe20c6982f3a0686e
Mikael Stenstrand <stenstrand.mikael@gmail.com>
2020-03-28 22:17:52 +0200

    Add keyboard CharacterMovement

---
+++ Assets/Scripts/Character/Movement/CharacterMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController2D))]
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float walkSpeed = 40f;
+
+    private CharacterController2D characterController;
+    private float horizontalMove = 0f;
+    private bool jump = false;
+
+    private void Awake()
+    {
+        characterController = gameObject.GetComponent<CharacterController2D>();
+    }
+
+    private void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * walkSpeed;
+        if (Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        characterController.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
