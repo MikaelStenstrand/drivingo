ec6347960296656846f6ec6e564dfd74ff96f3f0
Mikael Stenstrand <stenstrand.mikael@gmail.com>
2020-03-30 17:18:47 +0300

    Add capability of modifying Level settings through LevelSpec

---
+++ Assets/Scripts/Level/LevelSpec/LevelSpec.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Level Spec", menuName = "Specs/Level")]
+public class LevelSpec : ScriptableObject
+{
+    public CharacterSpec Character = null;
+    public FloatReference SpawnAmount = new FloatReference() { Value = 5.0f };
+    public FloatReference SpawnFrequencyInSeconds = new FloatReference() { Value = 5.0f };
+
+    // To be added later
+    // time countdown
+    // abilities[]
+}
