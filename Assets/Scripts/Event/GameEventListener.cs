16ddb8ebdc40f4d37fd07d0b7783fe0cec2752f8
Mikael Stenstrand <stenstrand.mikael@gmail.com>
2020-04-05 19:49:16 +0300

    add custom event system

---
+++ Assets/Scripts/Event/GameEventListener.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class GameEventListener : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameEvent gameEvent;
+
+    [SerializeField]
+    private UnityEventParams response;
+
+    private void OnEnable()
+    {
+        gameEvent.RegisterListener(this);
+    }
+
+    private void OnDisable()
+    {
+        if (gameEvent == null)
+        {
+            return;
+        }
+        gameEvent.UnregisterListener(this);
+    }
+
+    public void OnEventRaised(params object[] parameters)
+    {
+        response.Invoke(parameters);
+    }
+}
