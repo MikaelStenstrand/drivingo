7fd589ac0b8cefc68fbbf3bd84d9208fed24062d
Mikael Stenstrand <stenstrand.mikael@gmail.com>
2020-04-09 22:24:27 +0300

    UI for selecting Abilities (Toggle Ability List UI)

---
+++ Assets/src/Scripts/Ability/UI/Buttons/ToggleAbilityListUI.cs
@@ -0,0 +1,31 @@
+﻿using Doozy.Engine;
+using UnityEngine;
+
+public class ToggleAbilityListUI : MonoBehaviour
+{
+  private bool UIState = false;
+  private Animator animator;
+  private string ANIM_PARAM = "AbilityButtonState";
+
+  private void Start()
+  {
+    GameObject GO = transform.parent.gameObject;
+    animator = GO.GetComponentInChildren<Animator>();
+  }
+
+  public void ToggleUI()
+  {
+    UIState = UIState ? false : true;
+    if (UIState)
+    {
+      GameEventMessage.SendEvent("ShowAbilityListView");
+      animator.SetBool(ANIM_PARAM, UIState);
+    }
+    else
+    {
+      GameEventMessage.SendEvent("HideAbilityListView");
+      animator.SetBool(ANIM_PARAM, UIState);
+    }
+
+  }
+}
