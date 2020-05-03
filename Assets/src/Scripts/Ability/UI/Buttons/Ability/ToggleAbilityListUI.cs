3b5864df7e221dacc095052045b47d1bc765cef7
Mikael Stenstrand <stenstrand.mikael@gmail.com>
2020-05-03 14:57:14 +0300

    add UI elements for Ability Waypoint setup

---
+++ Assets/src/Scripts/Ability/UI/Buttons/Ability/ToggleAbilityListUI.cs
@@ -0,0 +1,33 @@
+﻿using Doozy.Engine;
+using UnityEngine;
+
+public class ToggleAbilityListUI : MonoBehaviour
+{
+    private bool UIState = false;
+    private Animator animator;
+    private string SHOW_ABILITY_LIST_VIEW_EVENT = "ShowAbilityListView";
+    private string HIDE_ABILITY_LIST_VIEW_EVENT = "HideAbilityListView";
+    private string ANIM_PARAM = "AbilityButtonState";
+
+    private void Start()
+    {
+        GameObject GO = transform.parent.gameObject;
+        animator = GO.GetComponentInChildren<Animator>();
+    }
+
+    public void ToggleUI()
+    {
+        UIState = UIState ? false : true;
+        if (UIState)
+        {
+            GameEventMessage.SendEvent(SHOW_ABILITY_LIST_VIEW_EVENT);
+            animator.SetBool(ANIM_PARAM, UIState);
+        }
+        else
+        {
+            GameEventMessage.SendEvent(HIDE_ABILITY_LIST_VIEW_EVENT);
+            animator.SetBool(ANIM_PARAM, UIState);
+        }
+
+    }
+}
---
+++ Assets/src/Scripts/Ability/UI/Buttons/Ability/Waypoint/ShowAbilityWaypointPlacementUIButton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Doozy.Engine;
+using UnityEngine;
+
+public class ShowAbilityWaypointPlacementUIButton : MonoBehaviour
+{
+    [SerializeField]
+    private BoolReference uiEnabler;
+
+    private string SHOW_ABILITY_WAYPOINT_VIEW_EVENT = "ShowAbilityWaypointView";
+    private string HIDE_ABILITY_WAYPOINT_VIEW_EVENT = "HideAbilityWaypointView";
+
+    private bool prevState = false;
+
+    private void Update()
+    {
+        if (uiEnabler.Value != prevState)
+        {
+            ToggleUI(uiEnabler.Value);
+        }
+    }
+
+    private void ToggleUI(bool value)
+    {
+        if (value)
+        {
+            GameEventMessage.SendEvent(SHOW_ABILITY_WAYPOINT_VIEW_EVENT);
+        }
+        else
+        {
+            GameEventMessage.SendEvent(HIDE_ABILITY_WAYPOINT_VIEW_EVENT);
+        }
+        prevState = uiEnabler.Value;
+    }
+}
